d6b83313d8a144d3d1d5fd5f6f234151be472b49
captainflorex <floriant.chaffray@edu.ece.fr>
2025-12-21 19:57:30 +0100

    Add function to read rotary encoder rotation

--- main.ino
+++ main.ino
@@ -9,3 +9,22 @@ const int PIN_ENCODEUR_CLK = 3;
 const int PIN_ENCODEUR_DT  = 4; 
 const int PIN_ENCODEUR_SW  = 2;  
 const int PIN_BOUTON_SW3   = A6; 
+
+const char listeCaracteres[] = "< abcdefghijklmnopqrstuvwxyz\x85\x82\x8A\x88\x87 ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,?!";
+const int tailleListe = sizeof(listeCaracteres) - 1;
+int indexCaractereSelectionne = 1; 
+
+unsigned long dernierAppuiBouton = 0;
+int dernierEtatClk;
+int lireRotationEncodeur();
+
+int lireRotationEncodeur() {
+  int etatClkActuel = digitalRead(PIN_ENCODEUR_CLK);
+  int resultat = 0;
+  if (etatClkActuel != dernierEtatClk && etatClkActuel == 1) {
+    if (digitalRead(PIN_ENCODEUR_DT) != etatClkActuel) resultat = 1;
+    else resultat = -1;
+  }
+  dernierEtatClk = etatClkActuel;
+  return resultat;
+}
